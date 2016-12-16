28d509a59e7037942189bf45e85816071b22310c
James Gorman <jamesgorman2@gmail.com>
2016-12-17 08:35:21 +1100

    Assert no multiple type definitions

---
+++ src/decorators/assert/__tests__/assertNoDuplicateTypes-test.js
@@ -0,0 +1,51 @@
+// @flow
+/* eslint-env jest */
+
+import {
+  Module,
+  ModuleRepository,
+} from '../../../config';
+
+import { FlattenedTypeGraph } from '../../../graph';
+
+import { assertNoDuplicateTypes } from '../assertNoDuplicateTypes';
+
+describe('assertNoDuplicateTypes', () => {
+  it('should do nothing when no duplicate types', () => {
+    const g = new FlattenedTypeGraph();
+    expect(assertNoDuplicateTypes(g)).toBe(g);
+  });
+  it('should ignore multiple extends', () => {
+    const g = FlattenedTypeGraph.from(
+      new ModuleRepository()
+        .withModule(
+          new Module('foo').withSchema('type Foo { id: ID name: Bar }')
+        )
+        .withModule(
+          new Module('bar').withSchema('extend type Foo { bar: Bar }')
+        )
+        .withModule(
+          new Module('baz').withSchema('extend type Foo { baz: Bar }')
+        )
+    );
+    expect(assertNoDuplicateTypes(g)).toBe(g);
+  });
+  it('should detect multiple declarations', () => {
+    const g = FlattenedTypeGraph.from(
+      new ModuleRepository()
+        .withModule(
+          new Module('foo').withSchema('type Foo { id: ID name: Bar }')
+        )
+        .withModule(
+          new Module('bar').withSchema('type Foo { bar: Bar }')
+        )
+        .withModule(
+          new Module('baz').withSchema('type Foo { baz: Bar }')
+        )
+    );
+    expect(assertNoDuplicateTypes(g).errors.map(e => e.message))
+      .toEqual([
+        'Type Foo declared more than once in modules foo, bar, baz.',
+      ]);
+  });
+});
---
+++ src/decorators/assert/assertNoDuplicateTypes.js
@@ -0,0 +1,23 @@
+// @flow
+
+import { AssertionError } from './AssertionError';
+import { errorInModules } from './errorInModules';
+
+import { FlattenedTypeGraph } from '../../graph';
+
+export function assertNoDuplicateTypes(graphIn: FlattenedTypeGraph): FlattenedTypeGraph {
+  return graphIn.types.values()
+    .filter(type => type.definitions.length > 1)
+    .reduce(
+      (graph, type) =>
+        graph.withError(
+          new AssertionError(
+            errorInModules(
+              `Type ${type.name} declared more than once`,
+              type.definitions.map(d => d.module)
+            )
+          )
+        ),
+      graphIn
+    );
+}
