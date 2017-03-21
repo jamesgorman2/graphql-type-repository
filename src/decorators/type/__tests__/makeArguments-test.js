2ccb1641bddfd2fec628a4bdf3595e41853c1a9c
James Gorman <gorman@Jamess-MacBook-Pro.local>
2017-03-22 06:29:12 +1100

    Add tests for making args and fields

---
+++ src/decorators/type/__tests__/makeArguments-test.js
@@ -0,0 +1,35 @@
+// @flow
+/* eslint-env jest */
+
+describe('makeArguments', () => {
+  it('should make single argument', () => {
+
+  });
+  it('should make multiple arguments', () => {
+
+  });
+  it('should add description from schema', () => {
+
+  });
+  it('should add description from config', () => {
+
+  });
+  it('should fail if description in schema and config', () => {
+
+  });
+  it('should add default value from schema', () => {
+
+  });
+  it('should add default value from config', () => {
+
+  });
+  it('should fail if default value in schema and config', () => {
+
+  });
+  it('should report multiple failures', () => {
+
+  });
+  it('should not error on bad argument type until final resolution', () => {
+
+  });
+});
---
+++ src/decorators/type/__tests__/makeFields-test.js
@@ -0,0 +1,35 @@
+// @flow
+/* eslint-env jest */
+
+describe('makeFields', () => {
+  it('should make single field', () => {
+
+  });
+  it('should make multiple fields', () => {
+
+  });
+  it('should add description from schema', () => {
+
+  });
+  it('should add description from config', () => {
+
+  });
+  it('should fail if description in schema and config', () => {
+
+  });
+  it('should add default value from schema', () => {
+
+  });
+  it('should add default value from config', () => {
+
+  });
+  it('should fail if default value in schema and config', () => {
+
+  });
+  it('should report multiple failures', () => {
+
+  });
+  it('should not error on bad field type until final resolution', () => {
+
+  });
+});
